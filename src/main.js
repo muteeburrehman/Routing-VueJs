f120d963ef976340a6c3c43b1d7a6ee85ea3cea5
muteeburrehman <muteeb285@gmail.com>
2023-10-12 20:01:32 +0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+    { path: '/users', component: UsersList },
+    { path: '/teams/:teamId', component: TeamMembers},
+
+  ]
+  
+});
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app');
